ba9f2ffedbdf57de19d0b8c1c06dda3cb0032557
ProskurinV <proskyrinv.g@gmail.com>
2022-09-24 00:08:16 +0300

    styled

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,41 @@
-import Profile from 'components/Profile/Profile';
-import user from 'user.json';
-import Statistics from './Statistics/Statistics';
-import data from 'data.json';
-import friends from 'friends.json';
-import FriendList from './FriendList/FriendList';
-import transactions from 'transactions.json';
-import TransactionHistory from './TransactionHistory/TransactionHistory';
+// import Profile from 'components/Profile/Profile';
+// import user from 'user.json';
+// import Statistics from './Statistics/Statistics';
+// import data from 'data.json';
+// import friends from 'friends.json';
+// import FriendList from './FriendList/FriendList';
+// import transactions from 'transactions.json';
+// import TransactionHistory from './TransactionHistory/TransactionHistory';
 
-export const App = () => {
+// export default function App() {
+//   return (
+//     <div>
+//       <Profile
+//         username={user.username}
+//         tag={user.tag}
+//         location={user.location}
+//         avatar={user.avatar}
+//         followers={user.stats.followers}
+//         views={user.stats.views}
+//         likes={user.stats.likes}
+//       />
+//       <Statistics title="Upload stats" stats={data} />
+//       <FriendList friends={friends} />
+//       <TransactionHistory items={transactions} />
+//     </div>
+//   );
+// }
+
+import styled from 'styled-components';
+
+const Text = styled.p`
+  color: red;
+`;
+
+export default function App() {
   return (
-    <div>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        followers={user.stats.followers}
-        views={user.stats.views}
-        likes={user.stats.likes}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </div>
+    <Text>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, amet!
+    </Text>
   );
-};
+}
